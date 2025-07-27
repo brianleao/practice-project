e94a1e5c853791863bde7026258a636b24d3c199
Brian Leao <brianleao@icloud.com>
2025-07-26 21:40:45 -0300

    add goal and summary implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header.tsx'
 import goalsImg from './assets/goals.jpg'
 import CourseGoals from './components/CourseGoals.tsx'
 import { useState } from 'react'
+import NewGoal from './components/NewGoal.tsx'
 
 export default function App() {
 
@@ -23,12 +24,17 @@ export default function App() {
   function handleDeleteGoal(id: number) {
     setGoals((prevGoals) => prevGoals.filter(g => g.id !== id));
   }
- 
+
+  function handleAddGoal(text: string, summary: string) {
+    setGoals((prevGoals) => prevGoals.concat({id: Math.random(), title: text, description: summary}));
+  } 
+
   return (
     <main>
       <Header image={{src: goalsImg, alt: "A list of goals"}}>
         <h1>Your course goals</h1>
       </Header>
+      <NewGoal onAdd={handleAddGoal}/>
       <CourseGoals goals={goals} onDelete={handleDeleteGoal}/>
     </main>
   )
--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,7 +1,38 @@
+interface NewGoalProps {
+    onAdd: (text: string, summary: string) => void;
+}
 
-export default function NewGoal() {
+import { useRef, type FormEvent } from "react";
+
+export default function NewGoal({onAdd}: NewGoalProps) {
+    const goalRef = useRef<HTMLInputElement>(null);
+    const summaryRef = useRef<HTMLInputElement>(null);
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const enteredGoal = goalRef.current!.value;
+        const enteredSummary = summaryRef.current!.value;
+
+        onAdd(enteredGoal, enteredSummary);
+
+        
+    }
+
+
     return (
-        <>
-        </>
+        <form onSubmit={handleSubmit}>
+            <p>
+                <label htmlFor="goal">Your Goal</label>
+                <input  id="goal" type="text" ref={goalRef}/>
+            </p>
+            <p>
+                <label htmlFor="summary">Short summary</label>
+                <input  id="summary" type="text" ref={summaryRef}/>
+            </p>
+            <p>
+                <button>Add Goal</button>
+            </p>
+        </form>
     )
 }
